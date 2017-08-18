8f0d096b5ae22d0437e33fb8c3fc2fd11abf8c04
Vivian <vcnales@gmail.com>
2017-08-18 18:27:27 -0500

    Added jQuery & Bootstrap script to body

--- public/index.php
+++ public/index.php
@@ -12,8 +12,14 @@ require_once __DIR__ . '/../bootstrap.php';
     <?php require '../views/partials/navbar.php'; ?>
 
     <?php require $mainView; ?>
-    
 
+
+
+	<!-- jQuery Version 1.11.1 -->
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
     <?php require '../views/partials/common_js.php'; ?>
 </body>
 </html>
