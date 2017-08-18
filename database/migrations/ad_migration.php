74103e24b933be4e8b9f5f502ebc7831c419e0ec
Vivian <vcnales@gmail.com>
2017-08-18 15:34:07 -0500

    Changed price data type from int to float

--- database/migrations/ad_migration.php
+++ database/migrations/ad_migration.php
@@ -8,7 +8,7 @@ $dbc->exec('DROP TABLE IF EXISTS ads');
 $query = 'CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     itemName VARCHAR(1000) NOT NULL,
-    price INT(10,2) NOT NULL,
+    price FLOAT(10,2) NOT NULL,
     description VARCHAR(2500) NOT NULL,
     sellerName VARCHAR(500) NOT NULL,
     username VARCHAR(500) NOT NULL,
