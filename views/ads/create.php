74ca125abc5d20de05f3c737ff034664522cfdd2
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-08-21 09:55:30 -0500

    Added addAnItem Function and added the form action and post method that was missing.

--- views/ads/create.php
+++ views/ads/create.php
@@ -1,23 +1,42 @@
 <!--Page for creating new advertisement listings-->
+<?php
 
-<form>
+function addAnItem() 
+{
+	$itemName = Input::get('itemName');
+	$price = Input::get('price');
+	$description = Input::get('description');
+	$sellerName = Input::get('sellerName');
+	$username = Input::get('username');
+	$imageUrl = Input::get('imageUrl');
+
+	var_dump($itemName);
+	var_dump($price);
+
+	if (empty($_POST)) {
+		var_dump("The field is empty.");
+	} else if (!empty($_POST)) {
+		Model::insert();
+	}
+}
+
+
+?>
+
+
+<form action ="adlister.dev/items/create" method="post">
 	<br>
 	<div class="col-md-6 col-md-offset-3">
 		<h2>Add a new item here!</h2>
 		<!-- Name -->
 	  	<div class="form-group">
-	    	<label for="name">Name</label>
-	    	<input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Name of the Item">
+	    	<label for="itemName">Name</label>
+	    	<input type="itemName" class="form-control" id="itemName" placeholder="Name of the Item">
 	  	</div>
 	  	<!-- Price -->
 	  	<div class="form-group">
 	    	<label for="price">Price</label>
-	   		<input type="price" class="form-control" id="exampleFormControlInput2" placeholder="Price of the Item ($)">
-	  	</div>
-	  	<!-- Seller Info -->
-	  	<div class="form-group">
-	    	<label for="sellerinfo">Seller Info</label>
-	   		<input type="sellerinfo" class="form-control" id="exampleFormControlInput3" placeholder="Seller Info">
+	   		<input type="price" class="form-control" id="price" placeholder="Price of the Item ($)">
 	  	</div>
 	  	<!-- Description -->
 		<div class="form-group">
@@ -25,12 +44,17 @@
 	    	<br>
 	    	<textarea class="form-control col-md-6-offset-3" id="description" placeholder="Descibe the item you are listing for sale" rows="3"></textarea>
 	  	</div>
+	  	<!-- Seller Info -->
+	  	<div class="form-group">
+	    	<label for="sellerName">Seller Info</label>
+	   		<input type="sellerName" class="form-control" id="sellerName" placeholder="Seller Info">
+	  	</div>
 	  	<!-- Photo File Input -->
 		<div class="form-group">
 	    	<label for="fileinput">Example file input</label>
-	    	<input type="file" class="form-control-file" id="fileinput">
+	    	<input type="fileinput" class="form-control-file" id="fileinput">
 	  	</div>
 	  	<!-- Submit Button -->
-		<button type="button" class="btn btn-default" >Post Item</button>
+		<button type="button" class="btn btn-default">Post Item</button>
 	</div>
 </form>
