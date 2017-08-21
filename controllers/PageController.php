9c3d77fcd5d9533f06b1471cd672024eeea27603
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-08-21 14:23:52 -0500

    Got the index page working! Added the escape function to the Input file.

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -57,13 +57,13 @@ function pageController()
         //     $mainView = '../views/ads/create.php'; 
         //     break;
 
-        case '/items/edit?id=':GET: edit account form / POST: update account with submission info
-             rerouteIfNotLoggedIn();
-             checkIfUserIdEntered();
-             editInputIfExisits();
-             $data['username'] = User::find(Input::get('id'));
-             $main_view = '../views/users/edit.php';
-            break;
+        // case '/items/edit?id=':GET: edit account form / POST: update account with submission info
+        //      rerouteIfNotLoggedIn();
+        //      checkIfUserIdEntered();
+        //      editInputIfExisits();
+        //      $data['username'] = User::find(Input::get('id'));
+        //      $main_view = '../views/users/edit.php';
+        //     break;
 
         default: // displays 404 if route not specified above
             $mainView = '../views/404.php';
