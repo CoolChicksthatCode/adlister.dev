69c4f7a72a236a77080e946fae7a1cac0e2b826e
Analyssa Martinez <analyssamartinez@Analyssas-MBP.satx.rr.com>
2017-08-20 22:11:41 -0500

    Ads/Edit page Bootstrap and form complete. Added Boostrap and correct item formatting to Ads/Index.php

--- views/ads/index.php
+++ views/ads/index.php
@@ -1,1 +1,46 @@
 <!--Page for an index of advertisements-->
+
+<!--<?php foreach($ads as $ad): ?>
+	<h5><?= Input::escape($ad->itemName) ?></h5>
+	<h5><?= Input::escape($ad->fileinput) ?></h5>
+	<h5><?= Input::escape($ad->description) ?></h5>
+	<h5><?= Input::escape($ad->sellerName) ?></h5>
+	<?php endforeach; ?>-->
+
+
+<h2 class="text-center">All items for sale</h2>
+<br>
+<div class="container">
+<div class="row">
+	<div class="col-md-4">
+	ITEM NAME
+	<br>
+	<img src="/img/placeholder.png" width="275" height="275">
+	</div>
+	<div class="col-md-4">
+	ITEM NAME
+	<br>
+	<img src="/img/placeholder.png" width="275" height="275">
+	</div>	
+	<div class="col-md-4">
+	ITEM NAME
+	<br>
+	<img src="/img/placeholder.png" width="275" height="275">
+	</div>	
+	<div class="col-md-4">
+	ITEM NAME
+	<br>
+	<img src="/img/placeholder.png" width="275" height="275">
+	</div>
+	<div class="col-md-4">
+	ITEM NAME
+	<br>
+	<img src="/img/placeholder.png" width="275" height="275">
+	</div>	
+	<div class="col-md-4">
+	ITEM NAME
+	<br>
+	<img src="/img/placeholder.png" width="275" height="275">
+	</div>	
+</div>
+</div>
