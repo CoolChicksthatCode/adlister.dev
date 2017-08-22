fb1f65e76a2a09f5dcdfe245dab3950cd01b2360
Vivian <vcnales@gmail.com>
2017-08-22 13:39:41 -0500

    Added condition for users logged in vs. users not logged in

--- views/ads/index.php
+++ views/ads/index.php
@@ -9,6 +9,7 @@ require_once "../utils/Input.php";
 
 $ads = Ads::all();
 
+var_dump($_SESSION);
 ?>
 
 <h1 class="text-center">All items for sale</h1>
@@ -18,7 +19,13 @@ $ads = Ads::all();
 		<?php foreach($ads as $ad): ?>
 		<div class="col-md-4" align="center">
 		<img src="/img/placeholder.png" width="250" height="250">
-		<a href="/items/show?id=<?=$ad->id;?>"><h5> Item Name: <?= Input::escape($ad->itemName) ?></h5></a>
+
+		<?php if(!empty($_SESSION)) : ?>
+			<a href="/items/show?id=<?=$ad->id;?>"><h5> Item Name: <?= Input::escape($ad->itemName) ?></h5></a>
+		<?php else: ?>			
+			<h5> Item Name: <?= Input::escape($ad->itemName) ?></h5>
+		<?php endif ?>
+
 		<h5> Item Description: <?= Input::escape($ad->description) ?></h5>
 		</div>
 		<?php endforeach; ?>
